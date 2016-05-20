5974fac09889ad4caeff2492b0ac9fdf5754e92e
rfecher <rfecher@gmail.com>
2016-05-20 15:45:32 -0400

    more maintainable - name the jar without version

--- provision.sh
+++ provision.sh
@@ -31,7 +31,7 @@ cd dev-resources
 mvn clean install
 cd ..
 echo "GeoWave: Building..."
-mvn clean package -pl examples -am -P geowave-tools-singlejar -DskipITs=true -DskipTests=true -Dfindbugs.skip=true -DskipFormat=true
+mvn clean package -pl examples -am -P geowave-tools-singlejar -DskipITs=true -DskipTests=true -Dfindbugs.skip=true -DskipFormat=true -Dtools.finalName=geowave-singlejar
 
 # Install GeoWave service
 echo "GeoWave: Installing Service..."
