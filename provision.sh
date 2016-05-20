f1184f4c43d6655630b46876265600b40f9bbfc3
rfecher <rfecher@gmail.com>
2016-05-20 13:42:56 -0400

    geowave-singlejar profile no longer exists

--- provision.sh
+++ provision.sh
@@ -31,7 +31,7 @@ cd dev-resources
 mvn clean install
 cd ..
 echo "GeoWave: Building..."
-mvn clean package -pl examples -am -P geowave-singlejar -DskipITs=true -DskipTests=true -Dfindbugs.skip=true -DskipFormat=true
+mvn clean package -pl examples -am -P geowave-tools-singlejar -DskipITs=true -DskipTests=true -Dfindbugs.skip=true -DskipFormat=true
 
 # Install GeoWave service
 echo "GeoWave: Installing Service..."
